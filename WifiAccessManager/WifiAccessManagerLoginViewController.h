4eb6e4bfa85e5f944c5290c573c908667b81b7c6
Brian Chen <brianchen8990@gmail.com>
2013-10-18 15:25:24 +0200

    init project with .gitignore file

---
+++ WifiAccessManager/WifiAccessManagerLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  WifiAccessManagerLoginViewController.h
+//  WifiAccessManager
+//
+//  Created by Brian Chen on 10/18/13.
+//  Copyright (c) 2013 Brian Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WifiAccessManagerLoginViewController : UIViewController
+
+@end
---
+++ WifiAccessManager/WifiAccessManagerLoginViewController.m
@@ -0,0 +1,38 @@
+//
+//  WifiAccessManagerLoginViewController.m
+//  WifiAccessManager
+//
+//  Created by Brian Chen on 10/18/13.
+//  Copyright (c) 2013 Brian Chen. All rights reserved.
+//
+
+#import "WifiAccessManagerLoginViewController.h"
+
+@interface WifiAccessManagerLoginViewController ()
+
+@end
+
+@implementation WifiAccessManagerLoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
